9e56cef3deff4be8cda98c2f575c7e1d0dbf878c
maurycywoznica <mjw11@wp.pl>
2022-09-11 07:04:13 +0200

    game changer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-Get
+const nav = document.querySelector('.navbar-collapse')
+document.addEventListener('click', () => {
+    if (nav.classList.contains('show')){
+        nav.classList.remove('show')
+    }
+})
